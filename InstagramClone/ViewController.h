10bb5d58b4c7e5276f236e1f94d00aad74993ccc
qaqaqaqaowh <nicholasowh@hotmail.com>
2017-12-06 16:29:18 +0800

    clean

---
+++ InstagramClone/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  InstagramClone
+//
+//  Created by NEXTAcademy on 12/6/17.
+//  Copyright © 2017 asd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ InstagramClone/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  InstagramClone
+//
+//  Created by NEXTAcademy on 12/6/17.
+//  Copyright © 2017 asd. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
